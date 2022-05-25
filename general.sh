37bcdc7c98be984a4a0aeed101aeaa572c949249
Omer Said Ozturk <omersaid@sabanciuniv.edu>
2022-05-25 04:35:15 +0300

    init

---
+++ general.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [[ `whoami` != root ]]
+then
+	echo [!] You need to run this script as root
+	echo [!] Trying to be root
+	sudo $0
+	exit
+fi
+
+# General init
+apt update -y
+apt upgrade -y
+
+# Run 
+apt install -y git
+apt install -y sudo
+apt install -y vim
+apt install -y nano
+apt install -y curl
+apt install -y wget
+
+apt install -y python3
+apt install -y python3-pip
+
+# For following scripts
+chmod -R +x * 
+
+WANNA_ALL=0
+
+WANNA_MONGO=0
+if [ $WANNA_MONGO = 1 ] || [ $WANNA_ALL = 1 ]
+then
+	echo [+] calling mongo install
+	./install_mongo
+else
+	echo [-] skipping mongo install
+fi
+
+WANNA_DOCKER=0
+if [ $WANNA_DOCKER = 1 ] || [ $WANNA_ALL = 1 ] 
+then
+	echo [+] calling docker install
+	./install_docker
+else
+	echo [-] skipping docker install
+fi
+
+WANNA_GO=False
+if [ $WANNA_GO = 1 ] || [ $WANNA_ALL = 1 ]
+then
+	echo [+] calling go install
+	./install_go
+else
+	echo [-] skipping go install
+fi
---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone https://github.com/omeroztrk/ubuntu-20-init-scripts
+cd ubuntu-20-init-scripts && chmod +x general.sh && ./general.sh
